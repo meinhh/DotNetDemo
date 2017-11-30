452f1c4b2665a31eb2d8ec084f6021ded57a25d7
Meirav Shenhar <meinhh@gmail.com>
2017-11-30 12:12:40 +0200

    test fails

--- TestDemo/HelloTest.cs
+++ TestDemo/HelloTest.cs
@@ -3,23 +3,23 @@ using ConsoleDemo;
 
 namespace TestDemo
 {
-    [TestClass]
-    public class HelloTest
-    {
-        private Hello _hello = new Hello();
+	[TestClass]
+	public class HelloTest
+	{
+		private Hello _hello = new Hello();
 
-        [TestMethod]
-        public void Hello_OnNameReceived_ShouldReturnMessageSayingHello()
-        {
-            var msg = _hello.GetMessage("fml");
-            Assert.AreEqual("Hello fml", msg);
-        }
+		[TestMethod]
+		public void Hello_OnNameReceived_ShouldReturnMessageSayingHello()
+		{
+			var msg = _hello.GetMessage("fml");
+			Assert.AreEqual("Hello fml", msg);
+		}
 
-        [TestMethod]
-        public void Hello_OnNull_ShouldReturnMessageSayingHello()
-        {
-            var msg = _hello.GetMessage(null);
-            Assert.AreEqual("Hello ", msg);
-        }
-    }
+		[TestMethod]
+		public void Hello_OnNull_ShouldReturnMessageSayingHello()
+		{
+			var msg = _hello.GetMessage(null);
+			Assert.AreEqual("Hello null", msg);
+		}
+	}
 }
